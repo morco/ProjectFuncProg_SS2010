f2751d48e580cf120e1d335698eaae2acb003a16
Mirko Wilhelmi <thegreatwiper@web.de>
2010-07-05 22:52:12 +0200

    Jetz abba

---
+++ currentState/Error.hs
@@ -0,0 +1,156 @@
+module Error where
+-- import BasicAlexMonad
+
+-- import ProgrammState -- leads to cyclcic imports!!
+
+import qualified Data.Map as M
+
+import Data.List 
+
+import Control.Monad.State
+
+-- import BasicHapMonad(ParserState(..))
+
+import Debug.Trace
+
+import ParserTypes(Token(..),Constant(..),TokenWrap(..),ParserState(..))
+
+-- data ParserState = ParserState { tokenList :: [TokenWrap], lineNumbers :: [Int], expectedLineNumbers :: [Int] }
+
+data RuleType
+    = LINE_NUMBER
+    | STRING_LITERAL
+    | STRING_VARIABLE
+    | FLOAT_VARIABLE
+    | INT_VARIABLE
+    | OPERATOR String
+    | COMMAND_INPUT
+    | STRING_NL_SURPRESSOR String
+ 
+    | CONSTANT
+    | INT_CONSTANT
+    | FLOAT_CONSTANT
+    | EXPRESSION
+    | NUMERICAL_EXPRESSION
+    | STRING_EXPRESSION
+    | BOOLEAN_EXPRESSION
+    | SYNTACTIC_ELEMENT String
+    | STRING_ASSIGNMENT
+    | INT_ASSIGNMENT
+    | FLOAT_ASSIGNMENT
+
+    | COMMAND
+    | VARIABLE
+    | LINE
+    deriving (Ord, Show, Eq)
+
+
+tokenToRuleType TkInput = COMMAND_INPUT
+tokenToRuleType (TkLineNumber _) = LINE_NUMBER
+tokenToRuleType (TkStringVar _) = STRING_VARIABLE
+tokenToRuleType (TkIntVar _) = INT_VARIABLE
+tokenToRuleType (TkFloatVar _) = FLOAT_VARIABLE
+tokenToRuleType (TkStringConcat) = STRING_NL_SURPRESSOR ";"
+tokenToRuleType (TkString _) = STRING_LITERAL
+
+{-
+expecting = 
+    M.fromList 
+    [
+     ((LINE_NUMBER,FLOAT_VARIABLE), [OPERATOR "="] ),
+     ((LINE_NUMBER,INT_VARIABLE), [OPERATOR "="] ),
+     ((LINE_NUMBER,STRING_VARIABLE), [OPERATOR "="] ),
+     ((LINE_NUMBER,STRING_VARIABLE), [OPERATOR "="] ),
+     ((WILDCARD,COMMAND_INPUT), [STRING_LITERAL,VARIABLE] )
+    ]
+-}
+
+expecting = 
+    M.fromList 
+    [
+     ([LINE_NUMBER], [COMMAND,VARIABLE] ),
+     ([FLOAT_VARIABLE,LINE_NUMBER], [OPERATOR "="] ),
+     ([INT_VARIABLE,LINE_NUMBER], [OPERATOR "="] ),
+     ([STRING_VARIABLE,LINE_NUMBER], [OPERATOR "="] ),
+     ([COMMAND_INPUT], [STRING_LITERAL,VARIABLE] ),
+     ([STRING_NL_SURPRESSOR ";",STRING_LITERAL,COMMAND_INPUT], [VARIABLE] ),
+     ([STRING_LITERAL,COMMAND_INPUT], [STRING_NL_SURPRESSOR ";",VARIABLE] )
+    ]
+
+
+context :: M.Map [RuleType] String
+context = 
+    M.fromList 
+    [
+     ([LINE_NUMBER,STRING_VARIABLE], "String Assignment" ),
+     ([LINE_NUMBER,FLOAT_VARIABLE], "Float Assignment" ),
+     ([LINE_NUMBER,INT_VARIABLE], "Int Assignment" ),
+     ([COMMAND_INPUT], "Input Command" )
+    ]
+
+
+{-
+singleContext =
+    M.fromList
+    [
+     (COMMAND_INPUT,True)
+    ]
+-}
+
+getExpectingMessage ertok = do
+    state <- get
+    let allToks = tokenList state -- "holeGesamteTokenList aus state" state -- TODO!!!
+    -- let preds = get2Predecessor ertok allToks
+    -- let expect = getMapVal $ M.lookup preds expecting
+    let (expect,cont) = getExpecting ertok allToks
+    return ("Expecting: " ++ (intercalate "or" $ map show expect), cont)
+
+  where
+     {-get2Predecessor tok tkList =
+          let elPos = getElemFirstPos tok 0 tkList
+          in case elPos of
+                  0 -> (WILDCARD,WILDCARD)
+                  1 -> let parent = tokenToRuleType (_token $ head tkList)                        
+                       in  (WILDCARD,parent)
+                  _ -> let parent = tokenToRuleType (_token $ tkList !! (elPos - 1)) 
+                       in case M.lookup parent singleContext of
+                               Just _ -> let grandparent = 
+                                                tokenToRuleType (_token $ tkList !! (elPos - 2))
+                                         in  (grandparent,parent)
+                               Nothing -> (WILDCARD,parent)
+
+
+     getElemFirstPos _ _ [] = error "Element was not in list!"
+     getElemFirstPos elem curPos (x:xs) = 
+         if elem == x
+           then curPos
+           else getElemFirstPos elem (curPos + 1) xs -}
+
+
+     getExpecting ertok tkList = 
+         let preds = reverse $ takeWhile ((/=) ertok) tkList
+         in getExpecting' preds []
+
+     getExpecting' [] key = ([LINE_NUMBER],"")
+     getExpecting' (x:xs) key = 
+         let newKey = key ++ [tokenToRuleType $ _token x]
+             exp = M.lookup newKey expecting
+         in case exp of
+                 Just x -> (x, getContext (reverse newKey) [])
+                 Nothing -> getExpecting' xs newKey
+
+     getContext [] _ = ""
+     getContext (x:xs) newKey =
+         let newKey' = newKey ++ [x]
+             -- cxt = trace (show newKey') $ M.lookup newKey' context
+             cxt = M.lookup newKey' context
+         in case cxt of
+                 Just x -> x
+                 Nothing -> getContext xs newKey'
+     
+
+
+getMapVal :: Maybe a -> a
+getMapVal (Just x) = x
+getMapVal _  = error "Var not found!"
+
---
+++ currentState/ParserTypes.hs
@@ -0,0 +1,189 @@
+module ParserTypes where
+ 
+-- This types are used from a variety of modules. Because of this and to 
+--  make the other modules smaller and cleaner i have de
+
+
+------------------------------ <Token Datatypes> ----------------------------
+
+data Token 
+     = TkLineNumber Int 
+
+------ <Reserved words> ---------------
+
+     | TkPrint  
+     | TkInput  
+     | TkFor   
+     | TkTo    
+     | TkNext 
+     | TkIf     
+     | TkThen   
+     | TkGoto   
+     | TkStep   
+     | TkLen    
+
+------ </Reserved words> ---------------
+
+------ <Combinators> ---------------
+
+     | TkSingleLineCommandCombinator  
+     | TkKomma  
+     | TkStringConcat  
+
+------ </Combinators> ---------------
+
+------ <Compare Operators> ---------------
+
+     | TkEqual  
+     | TkLt      
+     | TkGt      
+     | TkUnEqual 
+     | TkGE      
+     | TkLE      
+
+------ </Compare Operators> ---------------
+
+     | TkPlus  
+     | TkMinus 
+     | TkTimes 
+     | TkDiv   
+
+------ <Variables, Strings, Numbers> ---------------
+
+     | TkString String 
+     | TkConst  Constant 
+     | TkStringVar String 
+     | TkIntVar    String 
+     | TkFloatVar  String 
+
+------ </Variables, Strings, Numbers> --------------
+     | TkBracketOpen  
+     | TkBracketClose  
+
+     | TkLogOr  
+     | TkLogAnd  
+     | TkLogNeg  
+
+     | TkReturn  
+     | TkGoSub  
+     
+     | TkEnd  
+     
+     | TkGet  
+     | TkRandom  
+     | TkIntFunc  
+
+   deriving (Eq,Show)
+
+
+data Constant 
+     = TkIntConst Int  
+     | TkFloatConst Float
+   deriving (Eq, Show)
+
+
+data TokenWrap 
+    = TokenWrap { _type :: String, pos :: (Int,Int), _token :: Token }
+    | TkEOF
+    deriving (Show,Eq)
+
+------------------------------ </Token Datatypes> ---------------------------
+
+
+------------------------------ <Parser Datatypes> ---------------------------
+
+data ParserState 
+    = ParserState { 
+         tokenList :: [TokenWrap], 
+         lineNumbers :: [Int], 
+         expectedLineNumbers :: [Int] 
+      }
+
+
+data Command
+      = Command IOCommand
+      | ControlStructure ControlStruct
+      | Goto Int
+      | NOOP
+      | ArithAssignment NumVar NumExpr
+      | StringAssignment StringVar StringExpr
+      | Return
+      | End
+      deriving Show
+
+data StringExpr
+      = StringOp BasicString
+      | StringExpr (BasicString,BasicString) String
+      deriving Show
+
+data BasicString
+      = StringVar_BString StringVar
+      | StringLiteral String
+      deriving Show
+
+data NumFunction
+      = Len String
+      | LenVar StringVar
+      | Random Int
+      | IntFunc NumExpr
+      deriving Show
+
+
+data ControlStruct
+      = If BoolExpr [Command]
+      | For NumVar (Operand,Operand,Operand) [(Int,[Command])]
+      | GoSub Int 
+-- Ruecksprungpunkt ist ja schon durch die ZeilenNr des gosub Befehls gegeben, und muss eigentlich nicht nochmal extra aufgeschrieben werden
+      deriving Show
+
+
+data BoolExpr
+      = BoolExprString (StringExpr,StringExpr) String
+      | BoolExprNum (NumExpr,NumExpr) String
+      | BoolExprLog (BoolExpr,BoolExpr) String
+      deriving Show
+
+data IOCommand 
+      = Print ([Output], Bool)
+      | Input (InputStuff, Bool)
+      | Get Var
+      deriving Show
+
+data Output
+      = OutString String  
+      | OutVar Var
+      deriving Show
+
+data InputStuff 
+      = InputStuff [String] [Var]
+      deriving Show
+
+data NumExpr
+      = NumExpr (NumExpr,NumExpr) String
+      | NumOp Operand
+      | NumFunc NumFunction
+      deriving Show
+
+data Operand
+      = OpVar NumVar
+      | IntConst Int
+      | FloatConst Float
+      deriving Show
+
+data NumVar
+      = IntVar String
+      | FloatVar String
+      deriving (Eq, Show, Ord)
+
+data Var
+      = StringVar_Var StringVar
+      | NumVar_Var NumVar
+      deriving (Eq, Show, Ord)
+
+data StringVar
+      = StringVar String
+      deriving (Eq, Show, Ord)
+
+------------------------------ </Parser Datatypes> --------------------------
+
+
